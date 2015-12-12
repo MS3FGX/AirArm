aa77be7d6789bc04e7934bcc9f9115473f01f9a6
MS3FGX <MS3FGX@gmail.com>
2015-12-12 12:46:09 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+# Compile STLs for all .scad files
+
+# Create STL dir if it doesn't exist
+mkdir -p STL
+
+for i in *.scad
+do
+	echo -n "Building $i..."
+	openscad -o STL/$i.stl $i 2> /dev/null
+	echo "Done"
+done
